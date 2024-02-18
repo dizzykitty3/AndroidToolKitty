93286414a002ded97eaf10c244ee61ca4ce0ff99
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-02-19 07:16:36 +0800

    Refactor: Convert ClipboardUtils.java to .kt

---
+++ app/src/main/java/me/dizzykitty3/androidtoolkitty/ClipboardUtils.kt
@@ -0,0 +1,20 @@
+package me.dizzykitty3.androidtoolkitty
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+
+class ClipboardUtils(private val context: Context) {
+    fun clearClipboard() {
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        if (clipboard.hasPrimaryClip()) {
+            clipboard.clearPrimaryClip()
+        }
+    }
+
+    fun copyTextToClipboard(text: String?) {
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("label", text)
+        clipboard.setPrimaryClip(clip)
+    }
+}
