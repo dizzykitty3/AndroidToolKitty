43a8f114f683ae5f7af765249713519848e15181
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-02-28 11:44:27 +0800

    Convert MainActivity.java to MainActivity.kt

--- app/src/main/java/me/dizzykitty3/androidtoolkitty/MainActivity.kt
+++ app/src/main/java/me/dizzykitty3/androidtoolkitty/MainActivity.kt
@@ -1,118 +1,125 @@
-package me.dizzykitty3.androidtoolkitty;
+package me.dizzykitty3.androidtoolkitty
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import me.dizzykitty3.androidtoolkitty.Utils.convertUnicodeToCharacter
+import me.dizzykitty3.androidtoolkitty.Utils.debugLog
+import me.dizzykitty3.androidtoolkitty.Utils.openGoogleMaps
+import me.dizzykitty3.androidtoolkitty.Utils.showToast
+import me.dizzykitty3.androidtoolkitty.Utils.showToastAndRecordLog
+import me.dizzykitty3.androidtoolkitty.databinding.ActivityMainBinding
+import java.util.Objects
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    private var clipboardUtils: ClipboardUtils? = null
+    private var isAutoClearClipboard = false
+    private var sharedPreferences: SharedPreferences? = null
+    private var editor: SharedPreferences.Editor? = null
 
-import java.util.Objects;
-
-import me.dizzykitty3.androidtoolkitty.databinding.ActivityMainBinding;
-
-public class MainActivity extends AppCompatActivity {
-    private static final String CLIPBOARD_CLEARED = "clipboard cleared";
-    private static final String KEY_AUTO_CLEAR_CLIPBOARD = "key_auto_clear_clipboard";
-    private ActivityMainBinding binding;
-    private ClipboardUtils clipboardUtils;
-    private boolean isAutoClearClipboard;
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor editor;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        init();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
     }
 
-    private void init() {
-        initCore();
-        getSharedPreferencesValues();
-        initClipboardGroup();
-        initGoogleMapsGroup();
-        initUnicodeGroup();
-        initTestGroup();
+    private fun init() {
+        initCore()
+        sharedPreferencesValues
+        initClipboardGroup()
+        initGoogleMapsGroup()
+        initUnicodeGroup()
+        initTestGroup()
     }
 
-    private void initCore() {
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setLifecycleOwner(this);
-        sharedPreferences = getSharedPreferences("Android ToolKitty", MODE_PRIVATE);
-        editor = sharedPreferences.edit();
-        clipboardUtils = new ClipboardUtils(this);
+    private fun initCore() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding!!.setLifecycleOwner(this)
+        sharedPreferences = getSharedPreferences("Android ToolKitty", MODE_PRIVATE)
+        editor = sharedPreferences!!.edit()
+        clipboardUtils = ClipboardUtils(this)
     }
 
-    private void getSharedPreferencesValues() {
-        isAutoClearClipboard = sharedPreferences.getBoolean(KEY_AUTO_CLEAR_CLIPBOARD, false);
+    private val sharedPreferencesValues: Unit
+        get() {
+            isAutoClearClipboard = sharedPreferences!!.getBoolean(KEY_AUTO_CLEAR_CLIPBOARD, false)
+        }
+
+    private fun initClipboardGroup() {
+        binding!!.clearClipboardButton.setOnClickListener { _: View? ->
+            clipboardUtils!!.clearClipboard()
+            showToastAndRecordLog(this, CLIPBOARD_CLEARED)
+        }
+
+        binding!!.autoClearClipboardSettingSwitch.isChecked = isAutoClearClipboard
+        binding!!.autoClearClipboardSettingSwitch.setOnCheckedChangeListener { _, isChecked: Boolean ->
+            isAutoClearClipboard = isChecked
+            editor!!.putBoolean(KEY_AUTO_CLEAR_CLIPBOARD, isChecked)
+            editor!!.apply()
+            debugLog(if (isChecked) "auto clear clipboard is now: on" else "auto clear clipboard is now: off")
+        }
     }
 
-    private void initClipboardGroup() {
-        binding.clearClipboardButton.setOnClickListener(v -> {
-            clipboardUtils.clearClipboard();
-            Utils.showToastAndRecordLog(this, CLIPBOARD_CLEARED);
-        });
-
-        binding.autoClearClipboardSettingSwitch.setChecked(isAutoClearClipboard);
-        binding.autoClearClipboardSettingSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            isAutoClearClipboard = isChecked;
-            editor.putBoolean(KEY_AUTO_CLEAR_CLIPBOARD, isChecked);
-            editor.apply();
-            Utils.debugLog(isChecked ? "auto clear clipboard is now: on" : "auto clear clipboard is now: off");
-        });
+    private fun initGoogleMapsGroup() {
+        binding!!.openGoogleMapsButton.setOnClickListener { _: View? ->
+            val latitude = Objects.requireNonNull(binding!!.latitudeTextInput.text).toString()
+            val longitude = Objects.requireNonNull(binding!!.longitudeTextInput.text).toString()
+            openGoogleMaps(
+                this,
+                if (latitude.isEmpty()) "0" else latitude,
+                if (longitude.isEmpty()) "0" else longitude
+            )
+        }
     }
 
-    private void initGoogleMapsGroup() {
-        binding.openGoogleMapsButton.setOnClickListener(v -> {
-            String latitude = Objects.requireNonNull(binding.latitudeTextInput.getText()).toString();
-            String longitude = Objects.requireNonNull(binding.longitudeTextInput.getText()).toString();
-            Utils.openGoogleMaps(this, latitude.isEmpty() ? "0" : latitude, longitude.isEmpty() ? "0" : longitude);
-        });
-    }
-
-    private void initUnicodeGroup() {
-        binding.convertToCharacterButton.setOnClickListener(v -> {
-            final var unicode = Objects.requireNonNull(binding.unicodeTextInput.getText()).toString();
+    private fun initUnicodeGroup() {
+        binding!!.convertToCharacterButton.setOnClickListener { _: View? ->
+            val unicode = Objects.requireNonNull(binding!!.unicodeTextInput.text).toString()
             if (unicode.isEmpty()) {
-                return;
+                return@setOnClickListener
             }
             try {
-                var result = Utils.convertUnicodeToCharacter(unicode);
-                binding.unicodeOutputTextView.setText(result);
-                clipboardUtils.copyTextToClipboard(result);
-                Utils.showToast(this, result + " copied");
-            } catch (Exception e) {
-                Utils.showToast(this, e.getMessage() != null ? e.getMessage() : "Unknown error occurred");
+                val result = convertUnicodeToCharacter(unicode)
+                binding!!.unicodeOutputTextView.text = result
+                clipboardUtils!!.copyTextToClipboard(result)
+                showToast(this, "$result copied")
+            } catch (e: Exception) {
+                showToast(this, (if (e.message != null) e.message else "Unknown error occurred")!!)
             }
-        });
+        }
     }
 
-    private void initTestGroup() {
-        binding.testButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, TestActivity.class);
-            startActivity(intent);
-        });
+    private fun initTestGroup() {
+        binding!!.testButton.setOnClickListener { _: View? ->
+            val intent = Intent(this, TestActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        getSharedPreferencesValues();
+    override fun onResume() {
+        super.onResume()
+        sharedPreferencesValues
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
         if (hasFocus && isAutoClearClipboard) {
-            clipboardUtils.clearClipboard();
+            clipboardUtils!!.clearClipboard()
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    override fun onDestroy() {
+        super.onDestroy()
         if (Objects.nonNull(binding)) {
-            binding.unbind();
+            binding!!.unbind()
         }
+    }
+
+    companion object {
+        private const val CLIPBOARD_CLEARED = "clipboard cleared"
+        private const val KEY_AUTO_CLEAR_CLIPBOARD = "key_auto_clear_clipboard"
     }
 }
