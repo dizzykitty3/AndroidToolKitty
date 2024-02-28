aae753429a2d904844fdf91053e13a86622282bf
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-02-28 15:41:39 +0800

    Add application id suffix for debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
+
         release {
             isMinifyEnabled = false
             proguardFiles(
