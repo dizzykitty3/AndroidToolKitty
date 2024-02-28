b06fcfa61049728297757fc4239b1567e4c9dc21
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-02-28 11:40:02 +0800

    Convert Utils.java to Utils.kt

--- app/src/main/java/me/dizzykitty3/androidtoolkitty/Utils.kt
+++ app/src/main/java/me/dizzykitty3/androidtoolkitty/Utils.kt
@@ -1,78 +1,79 @@
-package me.dizzykitty3.androidtoolkitty;
+package me.dizzykitty3.androidtoolkitty
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
-import android.widget.Toast;
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+import java.util.Objects
 
-import androidx.annotation.NonNull;
+object Utils {
+    private var currentToast: Toast? = null
 
-import java.util.Objects;
-
-public class Utils {
-    private static Toast currentToast;
-
-    private Utils() {
-        // Empty
-    }
-
-    public static String convertUnicodeToCharacter(String unicode) throws Exception {
-        final var length = unicode.length();
-        if (length % 4 != 0) throw new Exception("The length of the input is not a multiple of 4");
+    @JvmStatic
+    @Throws(Exception::class)
+    fun convertUnicodeToCharacter(unicode: String): String {
+        val length = unicode.length
+        if (length % 4 != 0) throw Exception("The length of the input is not a multiple of 4")
         try {
-            final var stringBuilder = new StringBuilder();
-            for (int i = 0; i < length; i += 4) {
-                final var hexValue = unicode.substring(i, i + 4);
-                final var decimalValue = Integer.parseInt(hexValue, 16);
-                stringBuilder.append((char) decimalValue);
+            val stringBuilder = StringBuilder()
+            var i = 0
+            while (i < length) {
+                val hexValue = unicode.substring(i, i + 4)
+                val decimalValue = hexValue.toInt(16)
+                stringBuilder.append(decimalValue.toChar())
+                i += 4
             }
-            return stringBuilder.toString();
-        } catch (Exception e) {
-            throw new Exception("Invalid input");
+            return stringBuilder.toString()
+        } catch (e: Exception) {
+            throw Exception("Invalid input")
         }
     }
 
-    public static void openGoogleMaps(@NonNull Context context, @NonNull final String latitude, @NonNull final String longitude) {
-        final var coordinates = latitude + "," + longitude;
-        final var gmmIntentUri = Uri.parse("geo:" + coordinates + "?q=" + coordinates);
+    @JvmStatic
+    fun openGoogleMaps(context: Context, latitude: String, longitude: String) {
+        val coordinates = "$latitude,$longitude"
+        val gmmIntentUri = Uri.parse("geo:$coordinates?q=$coordinates")
 
-        final var mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
 
-        if (Objects.nonNull(mapIntent.resolveActivity(context.getPackageManager()))) {
-            context.startActivity(mapIntent);
-            return;
+        if (Objects.nonNull(mapIntent.resolveActivity(context.packageManager))) {
+            context.startActivity(mapIntent)
+            return
         }
-        Utils.showToastAndRecordLog(context, "Google Maps app is not installed");
-        openCertainAppOnPlayStore(context, "com.google.android.apps.maps");
+        showToastAndRecordLog(context, "Google Maps app is not installed")
+        openCertainAppOnPlayStore(context, "com.google.android.apps.maps")
     }
 
-    public static void showToastAndRecordLog(@NonNull Context context, @NonNull String event) {
-        debugLog(event);
-        showToast(context, event);
+    @JvmStatic
+    fun showToastAndRecordLog(context: Context, event: String) {
+        debugLog(event)
+        showToast(context, event)
     }
 
-    private static void openCertainAppOnPlayStore(@NonNull Context context, @NonNull final String packageName) {
-        final var playStoreUri = Uri.parse("market://details?id=" + packageName);
-        final var playStoreIntent = new Intent(Intent.ACTION_VIEW, playStoreUri);
+    private fun openCertainAppOnPlayStore(context: Context, packageName: String) {
+        val playStoreUri = Uri.parse("market://details?id=$packageName")
+        val playStoreIntent = Intent(Intent.ACTION_VIEW, playStoreUri)
 
-        if (Objects.nonNull(playStoreIntent.resolveActivity(context.getPackageManager()))) {
-            context.startActivity(playStoreIntent);
-            return;
+        if (Objects.nonNull(playStoreIntent.resolveActivity(context.packageManager))) {
+            context.startActivity(playStoreIntent)
+            return
         }
-        Utils.showToastAndRecordLog(context, "Google Play Store app is not installed");
+        showToastAndRecordLog(context, "Google Play Store app is not installed")
     }
 
-    public static void debugLog(@NonNull String logEvent) {
-        Log.d("me.dizzykitty3.androidtoolkitty", logEvent);
+    @JvmStatic
+    fun debugLog(logEvent: String) {
+        Log.d("me.dizzykitty3.androidtoolkitty", logEvent)
     }
 
-    public static void showToast(@NonNull Context context, @NonNull String toastText) {
+    @JvmStatic
+    fun showToast(context: Context, toastText: String) {
         if (Objects.nonNull(currentToast)) {
-            currentToast.cancel();
+            currentToast!!.cancel()
         }
-        currentToast = Toast.makeText(context, toastText, Toast.LENGTH_SHORT);
-        currentToast.show();
+        currentToast = Toast.makeText(context, toastText, Toast.LENGTH_SHORT)
+        currentToast!!.show()
     }
 }
