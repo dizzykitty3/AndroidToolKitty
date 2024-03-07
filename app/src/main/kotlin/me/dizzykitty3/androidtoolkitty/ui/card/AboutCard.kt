dd4da1596b27030de29acebaae6ac46b68c4e5ad
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-03-07 21:00:36 +0800

    Adjust UI

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/card/AboutCard.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/card/AboutCard.kt
@@ -2,10 +2,10 @@ package me.dizzykitty3.androidtoolkitty.ui.card
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Code
+import androidx.compose.material.icons.outlined.Schedule
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,22 +13,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.unit.dp
 import me.dizzykitty3.androidtoolkitty.R
 import me.dizzykitty3.androidtoolkitty.ui.component.CustomCard
+import me.dizzykitty3.androidtoolkitty.ui.component.CustomIconAndTextPagging
+import me.dizzykitty3.androidtoolkitty.ui.component.CustomSpacerPadding
 import me.dizzykitty3.androidtoolkitty.util.Utils.openUrl
 
+@Suppress("SpellCheckingInspection")
 @Composable
 fun AboutCard() {
     val c = LocalContext.current
     CustomCard(title = c.getString(R.string.about)) {
         val sourceCodeUrl = "https://github.com/dizzykitty3/android_toolkitty"
+        CustomSpacerPadding()
+        Row {
+            Icon(
+                imageVector = Icons.Outlined.AccountCircle,
+                contentDescription = null
+            )
+            CustomIconAndTextPagging()
+            Text(
+                text = "dizzykitty3",
+            )
+        }
+        CustomSpacerPadding()
         Row {
             Icon(
                 imageVector = Icons.Outlined.Code,
                 contentDescription = null
             )
-            Spacer(modifier = Modifier.padding(8.dp))
+            CustomIconAndTextPagging()
             Text(
                 text = c.getString(R.string.source_code_on_github),
                 color = MaterialTheme.colorScheme.primary,
@@ -36,6 +50,17 @@ fun AboutCard() {
                 modifier = Modifier.clickable {
                     openUrl(sourceCodeUrl)
                 }
+            )
+        }
+        CustomSpacerPadding()
+        Row {
+            Icon(
+                imageVector = Icons.Outlined.Schedule,
+                contentDescription = null
+            )
+            CustomIconAndTextPagging()
+            Text(
+                text = "version 20240307"
             )
         }
     }
