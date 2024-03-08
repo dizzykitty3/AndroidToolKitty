cc700c937c77588fd9e69e5e914d1288e8adb983
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-03-08 16:19:35 +0800

    Code style

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/card/TestCard.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/card/TestCard.kt
@@ -8,13 +8,13 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
 import me.dizzykitty3.androidtoolkitty.ui.component.CustomCard
 import me.dizzykitty3.androidtoolkitty.ui.component.CustomGradientText
 import me.dizzykitty3.androidtoolkitty.ui.component.CustomSpacerPadding
 
 @Composable
 fun TestCard() {
+    val text = "Android ToolKitty"
     CustomCard(
         title = "test", isExpand = false
     ) {
@@ -26,50 +26,29 @@ fun TestCard() {
             }
         )
         CustomSpacerPadding()
-        Text(
-            text = "Default 中文测试 日本語テスト",
-            fontFamily = FontFamily.Default
-        )
-        Text(
-            text = "SansSerif 中文测试 日本語テスト",
-            fontFamily = FontFamily.SansSerif
-        )
-        Text(
-            text = "Serif 中文测试 日本語テスト",
-            fontFamily = FontFamily.Serif
-        )
-        Text(
-            text = "Monospace 中文测试 日本語テスト",
-            fontFamily = FontFamily.Monospace
-        )
-        Text(
-            text = "Cursive 中文测试 日本語テスト",
-            fontFamily = FontFamily.Cursive
-        )
-        val ANDROID_TOOLKITTY = "Android ToolKitty"
         CustomGradientText(
-            textToDisplay = ANDROID_TOOLKITTY,
+            textToDisplay = text,
             colors = listOf(
                 Color(0xFF9796F0),
                 Color(0xFF9796F0)
             )
         )
         CustomGradientText(
-            textToDisplay = ANDROID_TOOLKITTY,
+            textToDisplay = text,
             colors = listOf(
                 Color(0xFFC9D6FF),
                 Color(0xFFE2E2E2)
             )
         )
         CustomGradientText(
-            textToDisplay = ANDROID_TOOLKITTY,
+            textToDisplay = text,
             colors = listOf(
                 Color(0xFF20002C),
                 Color(0xFFCBB4D4)
             )
         )
         CustomGradientText(
-            textToDisplay = ANDROID_TOOLKITTY,
+            textToDisplay = text,
             colors = listOf(
                 Color(0xFF4568DC),
                 Color(0xFFB06AB3)
