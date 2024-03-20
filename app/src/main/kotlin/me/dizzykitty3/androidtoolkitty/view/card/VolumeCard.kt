159c27148f33f1d0ce45142df146b4d0193fff42
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-03-20 11:09:56 +0800

    Improve volume card

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/view/card/VolumeCard.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/view/card/VolumeCard.kt
@@ -1,26 +1,27 @@
 package me.dizzykitty3.androidtoolkitty.view.card
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.VolumeUp
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import me.dizzykitty3.androidtoolkitty.R
 import me.dizzykitty3.androidtoolkitty.common.ui.component.CustomCard
+import me.dizzykitty3.androidtoolkitty.common.ui.component.CustomSpacerPadding
+import me.dizzykitty3.androidtoolkitty.common.ui.component.CustomTip
 import me.dizzykitty3.androidtoolkitty.common.util.AudioUtils
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VolumeCard() {
     val c = LocalContext.current
@@ -30,58 +31,48 @@ fun VolumeCard() {
         id = "card_volume"
     ) {
         val maxVolume = AudioUtils(c).getMaxVolumeIndex()
-        val mVolume = AudioUtils(c).getVolume()
-        var volume by remember { mutableIntStateOf(mVolume) }
-        val volumeRange = 0f..maxVolume.toFloat()
+        val options = listOf("0", "40%", "60%")
+        var selectedIndex by remember { mutableStateOf<Int?>(null) }
+        CustomTip(
+            text = c.getString(R.string.temp4)
+        )
         Text(
             text = c.getString(R.string.media_volume)
         )
-        Slider(
-            value = volume.toFloat(),
-            onValueChange = { newVolume ->
-                volume = newVolume.toInt()
-                AudioUtils(c).setVolume(newVolume.toInt())
-            },
-            valueRange = volumeRange,
-            steps = 4, // 4 steps = 5 options
-            colors = SliderDefaults.colors(
-                thumbColor = MaterialTheme.colorScheme.primary,
-                activeTrackColor = MaterialTheme.colorScheme.primary,
-                inactiveTrackColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)
-            )
-        )
-        Row(
+        CustomSpacerPadding()
+        SingleChoiceSegmentedButtonRow(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+            space = SegmentedButtonDefaults.BorderWidth
         ) {
-            Button(
-                onClick = {
-                    AudioUtils(c).setVolume(0)
+            options.forEachIndexed { index, label ->
+                SegmentedButton(
+                    onClick = {
+                        selectedIndex = index
+                        when (index) {
+                            0 -> {
+                                AudioUtils(c).setVolume(0)
+                            }
+
+                            1 -> {
+                                AudioUtils(c).setVolume((0.4 * maxVolume).toInt())
+                            }
+
+                            2 -> {
+                                AudioUtils(c).setVolume((0.6 * maxVolume).toInt())
+                            }
+                        }
+                    },
+                    selected = index == selectedIndex,
+                    shape = SegmentedButtonDefaults.itemShape(
+                        index = index,
+                        count = options.size
+                    )
+                ) {
+                    Text(
+                        text = label
+                    )
                 }
-            ) {
-                Text(
-                    text = "0%"
-                )
-            }
-            Button(
-                onClick = {
-                    AudioUtils(c).setVolume((0.2 * maxVolume).toInt())
-                }
-            ) {
-                Text(
-                    text = "20%"
-                )
-            }
-            Button(
-                onClick = {
-                    AudioUtils(c).setVolume((0.4 * maxVolume).toInt())
-                }
-            ) {
-                Text(
-                    text = "40%"
-                )
             }
         }
-
     }
 }
