3d4ae7817527b5eaf05bd89a9dd4505661097e8b
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-03-20 15:21:56 +0800

    Support Android 11 by adding color scheme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "me.dizzykitty3.androidtoolkitty"
-        minSdk = 31
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
