45eacb04b451b9c1e7fd16b5d7c5d9dcab7b9f8d
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-03-22 12:10:45 +0800

    Add restart activity after turning on/off dynamic color

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/view/card/SettingsCard.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/view/card/SettingsCard.kt
@@ -1,6 +1,7 @@
 package me.dizzykitty3.androidtoolkitty.view.card
 
 import android.app.Activity
+import android.content.Intent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,11 +17,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import me.dizzykitty3.androidtoolkitty.MainActivity
 import me.dizzykitty3.androidtoolkitty.R
 import me.dizzykitty3.androidtoolkitty.common.ui.component.CustomAlertDialogButton
 import me.dizzykitty3.androidtoolkitty.common.ui.component.CustomNoIconCard
 import me.dizzykitty3.androidtoolkitty.common.ui.component.CustomSpacerPadding
 import me.dizzykitty3.androidtoolkitty.viewmodel.SettingsViewModel
+
 
 @Composable
 fun SettingsCard() {
@@ -82,6 +85,10 @@ fun SettingsCard() {
                 onCheckedChange = {
                     mIsDynamicColor = it
                     SettingsViewModel().setIsDynamicColor(c, it)
+                    val intent = Intent(c, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    c.startActivity(intent)
+                    (c as Activity).finish()
                 }
             )
         }
