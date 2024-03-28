434edc2aa945c1202c44fead278472a847d87c8d
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-03-28 16:10:26 +0800

    add edit custom volume label feature

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/view/card/VolumeCard.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/view/card/VolumeCard.kt
@@ -2,11 +2,14 @@ package me.dizzykitty3.androidtoolkitty.view.card
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.VolumeUp
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
@@ -23,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.input.ImeAction
 import me.dizzykitty3.androidtoolkitty.R
 import me.dizzykitty3.androidtoolkitty.common.ui.component.CustomCard
 import me.dizzykitty3.androidtoolkitty.common.ui.component.CustomSpacerPadding
@@ -44,12 +48,13 @@ fun VolumeCard() {
         var mCustomVolume by remember { mutableIntStateOf(customVolume) }
 
         val customVolumeOptionLabel = SettingsViewModel().getCustomVolumeOptionLabel(c)
+        var mCustomVolumeOptionLabel by remember { mutableStateOf(customVolumeOptionLabel) }
 
         val options = listOf(
             c.getString(R.string.mute),
             "40%",
             "60%",
-            if (customVolumeOptionLabel != "") customVolumeOptionLabel
+            if (mCustomVolumeOptionLabel != "") mCustomVolumeOptionLabel
             else {
                 if (mCustomVolume == -1) "+"
                 else "${mCustomVolume}%"
@@ -66,7 +71,9 @@ fun VolumeCard() {
             modifier = Modifier.fillMaxWidth(),
             space = SegmentedButtonDefaults.BorderWidth
         ) {
-            var showDialog by remember { mutableStateOf(false) }
+            var showVolumeDialog by remember { mutableStateOf(false) }
+
+            var showVolumeOptionLabelDialog by remember { mutableStateOf(false) }
 
             options.forEachIndexed { index, label ->
                 SegmentedButton(
@@ -89,7 +96,7 @@ fun VolumeCard() {
                                 if (mCustomVolume != -1) {
                                     AudioUtils(c).setVolume((mCustomVolume * 0.01 * maxVolume).toInt())
                                 } else
-                                    showDialog = true
+                                    showVolumeDialog = true
                             }
                         }
                     },
@@ -103,12 +110,12 @@ fun VolumeCard() {
                 }
             }
 
-            if (showDialog) {
+            if (showVolumeDialog) {
                 var newCustomVolume by remember { mutableFloatStateOf(0f) }
 
                 AlertDialog(
                     onDismissRequest = {
-                        showDialog = false
+                        showVolumeDialog = false
                     },
                     title = {
                         Text(text = "${c.getString(R.string.add_custom_volume)}\n${newCustomVolume.toInt()}% -> ${(newCustomVolume * 0.01 * maxVolume).toInt()}/$maxVolume")
@@ -131,7 +138,7 @@ fun VolumeCard() {
                                 } else {
                                     SettingsViewModel().setCustomVolume(c, newCustomVolume.toInt())
                                     mCustomVolume = newCustomVolume.toInt()
-                                    showDialog = false
+                                    showVolumeOptionLabelDialog = true
                                     AudioUtils(c).setVolume((mCustomVolume * 0.01 * maxVolume).toInt())
                                 }
                             }
@@ -141,7 +148,7 @@ fun VolumeCard() {
                     },
                     dismissButton = {
                         TextButton(
-                            onClick = { showDialog = false }
+                            onClick = { showVolumeDialog = false }
                         ) {
                             Text(text = c.getString(android.R.string.cancel))
                         }
@@ -149,6 +156,73 @@ fun VolumeCard() {
                     modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_dialog))
                 )
             }
+
+            if (showVolumeOptionLabelDialog) {
+                var optionLabel by remember { mutableStateOf("") }
+
+                AlertDialog(
+                    onDismissRequest = {
+                        showVolumeOptionLabelDialog = false
+                    },
+                    title = {
+                        Text(text = "You can set a label for it")
+                    },
+                    text = {
+                        OutlinedTextField(
+                            value = optionLabel,
+                            onValueChange = { optionLabel = it },
+                            label = { Text(text = "Option label") },
+                            modifier = Modifier.fillMaxWidth(),
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                imeAction = ImeAction.Done
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onDone = {
+                                    SettingsViewModel().setCustomVolumeOptionLabel(
+                                        c,
+                                        optionLabel
+                                    )
+                                }
+                            ),
+                            placeholder = { Text(text = "optional") }
+                        )
+                    },
+                    confirmButton = {
+                        Button(
+                            onClick = {
+                                SettingsViewModel().setCustomVolumeOptionLabel(c, optionLabel)
+                                mCustomVolumeOptionLabel = optionLabel
+                                showVolumeOptionLabelDialog = false
+                                showVolumeDialog = false
+                            }
+                        ) {
+                            Text(text = c.getString(android.R.string.ok))
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(
+                            onClick = {
+                                showVolumeOptionLabelDialog = false
+                                showVolumeDialog = false
+                            }
+                        ) {
+                            Text(text = c.getString(android.R.string.cancel))
+                        }
+                    },
+                    modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_dialog))
+                )
+            }
+        }
+
+        Button(
+            onClick = {
+                mCustomVolume = -1
+                mCustomVolumeOptionLabel = ""
+                SettingsViewModel().setCustomVolume(c, -1)
+                SettingsViewModel().setCustomVolumeOptionLabel(c, "")
+            }
+        ) {
+            Text(text = "test: reset")
         }
     }
 }
