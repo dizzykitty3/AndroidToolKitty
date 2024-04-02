16647d51ca7e8c991af3635d1677a6856b608bf7
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-04-02 13:08:57 +0800

    fix: intent param

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/common/util/IntentUtils.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/common/util/IntentUtils.kt
@@ -26,15 +26,15 @@ class IntentUtils(private val context: Context) {
     @SuppressLint("InlinedApi")
     fun openSystemSettings(settingType: String) {
         val intent: Intent = when (settingType) {
-            "display" -> Intent(Settings.ACTION_DISPLAY_SETTINGS)
-            "auto_rotate" -> if (OsVersion.android12()) Intent(Settings.ACTION_AUTO_ROTATE_SETTINGS) else return
-            "locale" -> Intent(Settings.ACTION_LOCALE_SETTINGS)
-            "manage_default_apps" -> Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS)
-            "bluetooth" -> Intent(Settings.ACTION_BLUETOOTH_SETTINGS)
-            "date" -> Intent(Settings.ACTION_DATE_SETTINGS)
-            "ignore_battery_optimization" -> Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
-            "development_settings" -> Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
-            "captioning" -> Intent(Settings.ACTION_CAPTIONING_SETTINGS)
+            SETTING_1 -> Intent(Settings.ACTION_DISPLAY_SETTINGS)
+            SETTING_2 -> if (OsVersion.android12()) Intent(Settings.ACTION_AUTO_ROTATE_SETTINGS) else return
+            SETTING_3 -> Intent(Settings.ACTION_BLUETOOTH_SETTINGS)
+            SETTING_4 -> Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS)
+            SETTING_5 -> Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
+            SETTING_6 -> Intent(Settings.ACTION_CAPTIONING_SETTINGS)
+            SETTING_7 -> Intent(Settings.ACTION_LOCALE_SETTINGS)
+            SETTING_8 -> Intent(Settings.ACTION_DATE_SETTINGS)
+            SETTING_9 -> Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
             else -> return
         }
 
@@ -114,5 +114,14 @@ class IntentUtils(private val context: Context) {
         private const val HTTPS = "https://"
         private const val GOOGLE_MAPS = "com.google.android.apps.maps"
         private const val GOOGLE_PLAY = "com.android.vending"
+        private const val SETTING_1 = "setting_display"
+        private const val SETTING_2 = "setting_auto_rotate"
+        private const val SETTING_3 = "setting_bluetooth"
+        private const val SETTING_4 = "setting_default_apps"
+        private const val SETTING_5 = "setting_battery_optimization"
+        private const val SETTING_6 = "setting_caption"
+        private const val SETTING_7 = "setting_locale"
+        private const val SETTING_8 = "setting_date_and_time"
+        private const val SETTING_9 = "setting_developer"
     }
 }
