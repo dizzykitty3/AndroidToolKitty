cfed4598fd2ffc6f06c904c03ffd603f43ddfe9e
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-04-09 16:34:12 +0800

    refactor: drop context param
    - use public val app to replace the freaking context param everywhere

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/view/card/SettingsCard.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/view/card/SettingsCard.kt
@@ -20,12 +20,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.navigation.NavHostController
 import me.dizzykitty3.androidtoolkitty.R
+import me.dizzykitty3.androidtoolkitty.ToolKittyApp.Companion.app
 import me.dizzykitty3.androidtoolkitty.foundation.ui_component.CustomAlertDialogButton
 import me.dizzykitty3.androidtoolkitty.foundation.ui_component.CustomBoldText
 import me.dizzykitty3.androidtoolkitty.foundation.ui_component.CustomCard
@@ -44,7 +44,6 @@ fun SettingsCard(navController: NavHostController) {
     CustomCard(
         title = R.string.settings
     ) {
-        val context = LocalContext.current
         val view = LocalView.current
 
         val settingsViewModel = remember { SettingsViewModel }
@@ -176,7 +175,7 @@ fun SettingsCard(navController: NavHostController) {
             negativeButtonText = null,
             onClickAction = {
                 settingsViewModel.clear()
-                (context as Activity).finish()
+                (app.baseContext as Activity).finish()
             }
         )
     }
