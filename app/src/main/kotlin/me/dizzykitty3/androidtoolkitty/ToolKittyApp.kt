0f87f96f980959bc0cb49896a1528ae69a896068
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-04-10 21:22:42 +0800

    fix: app crash
    - fix crash: java.lang.IllegalStateException: Cannot access database on the main thread since it may potentially lock the UI for a long period of time

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ToolKittyApp.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ToolKittyApp.kt
@@ -2,14 +2,10 @@ package me.dizzykitty3.androidtoolkitty
 
 import android.app.Application
 import android.util.Log
-import androidx.room.Room
-import me.dizzykitty3.androidtoolkitty.data.room.database.ToolKittyDatabase
-import me.dizzykitty3.androidtoolkitty.data.room.entity.UrlHistory
 
 class ToolKittyApp : Application() {
     companion object {
         private const val TAG = "ToolKittyApp"
-        private const val ROOM_DATABASE_NAME = "ToolKittyDatabase"
         lateinit var app: ToolKittyApp private set
     }
 
@@ -17,13 +13,5 @@ class ToolKittyApp : Application() {
         super.onCreate()
         Log.d(TAG, "onCreate")
         app = this
-
-        val db = Room.databaseBuilder(
-            applicationContext,
-            ToolKittyDatabase::class.java, ROOM_DATABASE_NAME
-        ).build()
-
-        val urlHistoryDao = db.urlHistoryDao()
-        val urlHistories: List<UrlHistory> = urlHistoryDao.getAll()
     }
 }
