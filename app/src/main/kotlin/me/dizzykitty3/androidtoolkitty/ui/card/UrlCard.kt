4a2ff3fcffb8a1117decd466996584ead9d7cec4
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-04-12 09:37:25 +0800

    refactor: drop unnecessary variable
    - drop unnecessary context variable

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/card/UrlCard.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/card/UrlCard.kt
@@ -19,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
@@ -101,8 +100,6 @@ fun UrlCard() {
 
         CustomGroupDivider()
         CustomGroupTitleText(resId = R.string.social_media_profile)
-
-        val context = LocalContext.current
 
         var username by remember { mutableStateOf("") }
 
