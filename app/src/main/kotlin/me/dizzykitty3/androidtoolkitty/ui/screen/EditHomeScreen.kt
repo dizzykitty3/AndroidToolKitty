9be2eff0780ba6bb8433905026da5574740a1b84
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-04-21 20:31:44 +0800

    fix(lazy column): fix lazy columns
    - stop using @composable custom screen

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/screen/EditHomeScreen.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/screen/EditHomeScreen.kt
@@ -1,21 +1,35 @@
 package me.dizzykitty3.androidtoolkitty.ui.screen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import me.dizzykitty3.androidtoolkitty.R
 import me.dizzykitty3.androidtoolkitty.data.sharedpreferences.SettingsSharedPref
 import me.dizzykitty3.androidtoolkitty.foundation.const.CARD_6
-import me.dizzykitty3.androidtoolkitty.foundation.ui.component.CustomScreen
+import me.dizzykitty3.androidtoolkitty.foundation.ui.component.CustomBottomPadding
+import me.dizzykitty3.androidtoolkitty.foundation.ui.component.CustomTopPadding
 import me.dizzykitty3.androidtoolkitty.ui.card.EditHomeCard
 import me.dizzykitty3.androidtoolkitty.ui.card.EditSysSettingCard
 
 @Composable
 fun EditHomeScreen() {
-    CustomScreen {
-        EditHomeCard()
+    val cardPadding = dimensionResource(id = R.dimen.padding_card_content)
+    val sysSettingCard by remember { mutableStateOf(SettingsSharedPref.getCardShowedState(CARD_6)) }
 
-        val sysSettingCard by remember { mutableStateOf(SettingsSharedPref.getCardShowedState(CARD_6)) }
-        if (sysSettingCard) EditSysSettingCard()
+    LazyColumn(
+        modifier = Modifier.padding(
+            start = cardPadding,
+            end = cardPadding
+        )
+    ) {
+        item { CustomTopPadding() }
+        item { EditHomeCard() }
+        if (sysSettingCard) item { EditSysSettingCard() }
+        item { CustomBottomPadding() }
     }
 }
--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/screen/PermissionRequestScreen.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/screen/PermissionRequestScreen.kt
@@ -1,12 +1,27 @@
 package me.dizzykitty3.androidtoolkitty.ui.screen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import me.dizzykitty3.androidtoolkitty.foundation.ui.component.CustomScreen
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import me.dizzykitty3.androidtoolkitty.R
+import me.dizzykitty3.androidtoolkitty.foundation.ui.component.CustomBottomPadding
+import me.dizzykitty3.androidtoolkitty.foundation.ui.component.CustomTopPadding
 import me.dizzykitty3.androidtoolkitty.ui.card.PermissionRequestCard
 
 @Composable
 fun PermissionRequestScreen() {
-    CustomScreen {
-        PermissionRequestCard()
+    val cardPadding = dimensionResource(id = R.dimen.padding_card_content)
+
+    LazyColumn(
+        modifier = Modifier.padding(
+            start = cardPadding,
+            end = cardPadding
+        )
+    ) {
+        item { CustomTopPadding() }
+        item { PermissionRequestCard() }
+        item { CustomBottomPadding() }
     }
 }
