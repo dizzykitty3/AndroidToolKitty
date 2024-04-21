cabf943ab196804f0e8b00ec0d1ad4b3170e2251
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-04-21 20:52:47 +0800

    move CustomScreen.kt

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/screen/CustomScreen.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/screen/CustomScreen.kt
@@ -1,4 +1,4 @@
-package me.dizzykitty3.androidtoolkitty.foundation.ui.component
+package me.dizzykitty3.androidtoolkitty.ui.screen
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -6,9 +6,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import me.dizzykitty3.androidtoolkitty.R
+import me.dizzykitty3.androidtoolkitty.foundation.ui.component.CustomBottomPadding
+import me.dizzykitty3.androidtoolkitty.foundation.ui.component.CustomTopPadding
 
+/**
+ * Example @Composable function of screens.
+ */
+@Suppress("unused")
 @Composable
-fun CustomScreen(
+fun ExampleScreen(
     cardContent: @Composable () -> Unit
 ) {
     val cardPadding = dimensionResource(id = R.dimen.padding_card_content)
