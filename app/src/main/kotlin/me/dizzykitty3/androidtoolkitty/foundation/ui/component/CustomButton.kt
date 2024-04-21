28e11aae15fa6623dc42a343a94daf6b54bb04a0
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-04-21 20:47:28 +0800

    style(comment): code style
    - add a period

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/foundation/ui/component/CustomButton.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/foundation/ui/component/CustomButton.kt
@@ -36,7 +36,7 @@ fun CustomSystemSettingsButton(
 }
 
 /**
- * trailingIcon param of a @Composable TextField, to clear the user input
+ * trailingIcon param of a @Composable TextField, to clear the user input.
  */
 @Composable
 fun ClearInput(
