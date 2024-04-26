b990e47c976f80340c052bfea568684063b12bbd
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-04-26 16:04:40 +0800

    add kdoc for function toast

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/foundation/util/ToastUtil.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/foundation/util/ToastUtil.kt
@@ -5,10 +5,18 @@ import androidx.annotation.StringRes
 import me.dizzykitty3.androidtoolkitty.MainApp.Companion.app
 
 object ToastUtil {
+    /**
+     * NOT RECOMMENDED. Use Snackbar instead.
+     * @see SnackbarUtil.snackbar
+     */
     @JvmStatic
     fun toast(message: String) =
         Toast.makeText(app.applicationContext, message, Toast.LENGTH_SHORT).show()
 
+    /**
+     * NOT RECOMMENDED. Use Snackbar instead.
+     * @see SnackbarUtil.snackbar
+     */
     @JvmStatic
     fun toast(@StringRes message: Int) = toast(app.applicationContext.getString(message))
 }
