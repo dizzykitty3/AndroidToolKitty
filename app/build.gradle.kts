e13f1ebbd013d72e96e1d5077854bb9b288a3f7a
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-05-08 15:09:06 +0800

    app common: min sdk 29 -> 26
    - min android version support: android 10 -> android 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "me.dizzykitty3.androidtoolkitty"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
