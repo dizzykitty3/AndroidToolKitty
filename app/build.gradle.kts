f7996537404e556a61dbc629132d0981a8a203d3
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-05-20 22:17:53 +0800

    [IMPROVE] update color theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "me.dizzykitty3.androidtoolkitty"
         minSdk = libs.versions.minSDK.get().toInt()
         targetSdk = libs.versions.targetSDK.get().toInt()
-        versionCode = 705
-        versionName = "1.0.705"
+        versionCode = 706
+        versionName = "1.0.706"
 
         resValue("string", "app_name", "ToolKitty")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
