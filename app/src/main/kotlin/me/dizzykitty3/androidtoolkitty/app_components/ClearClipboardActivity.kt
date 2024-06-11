1706bba5939f55b78ee3c93ef09a9c510cb06e65
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-06-11 14:53:03 +0800

    Fix "Clear clipboard" Tile Service

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/app_components/ClearClipboardActivity.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/app_components/ClearClipboardActivity.kt
@@ -11,14 +11,24 @@ class ClearClipboardActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.d("onCreate")
-        val cleared = ClipboardUtil.check()
-        if (cleared) {
-            Timber.i("clipboard cleared")
-            Toast.makeText(this, getString(R.string.clipboard_cleared), Toast.LENGTH_SHORT).show()
-        } else {
-            Timber.i("clipboard is empty")
-            Toast.makeText(this, getString(R.string.clipboard_is_empty), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        Timber.d("onWindowFocusChanged")
+        if (hasFocus) {
+            Timber.d("hasFocus")
+            val cleared = ClipboardUtil.check()
+            if (cleared) {
+                Timber.i("clipboard cleared")
+                Toast.makeText(this, getString(R.string.clipboard_cleared), Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Timber.i("clipboard is empty")
+                Toast.makeText(this, getString(R.string.clipboard_is_empty), Toast.LENGTH_SHORT)
+                    .show()
+            }
+            finish()
         }
-        finish()
     }
 }
