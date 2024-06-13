6d1971f0baab1ad02704edd6df959292b54f1d53
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-06-13 18:07:09 +0800

    string res

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/settings_screen/SettingsScreen.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/ui/settings_screen/SettingsScreen.kt
@@ -2,6 +2,7 @@ package me.dizzykitty3.androidtoolkitty.ui.settings_screen
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import me.dizzykitty3.androidtoolkitty.R
 import me.dizzykitty3.androidtoolkitty.ui_components.CustomCard
 import me.dizzykitty3.androidtoolkitty.ui_components.CustomScreen
 import me.dizzykitty3.androidtoolkitty.ui_components.CustomTip
@@ -13,8 +14,8 @@ fun SettingsScreen(navController: NavHostController) {
         Appearance()
         General()
         Customize(navController = navController)
-        CustomCard(title = "Online features") {
-            CustomTip("Service Provider Data Privacy Disclaimer")
+        CustomCard(titleRes = R.string.online_features) {
+            CustomTip(R.string.service_provider_privacy_disclaimer)
             UserSyncSection()
             GroupDivider()
             RuleUpdateSection()
