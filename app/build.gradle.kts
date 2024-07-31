8bcaca7a79f3dab0ff0fa0fbde493540834c3e58
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-07-31 16:33:25 +0800

    release version 1.1 (july 2024)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.dizzykitty3.androidtoolkitty"
         minSdk = 21
         targetSdk = compileSdk
-        versionCode = 966
-        versionName = "1.0.${versionCode}"
+        versionCode = 1100
+        versionName = "1.1"
 
         resValue("string", "app_name", "ToolKitty")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
