9ca8f7d258381994c529704a05d4d8520d997541
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-09-14 10:11:06 +0800

    build: remove enableStrongSkippingMode = true
    Enable strong skipping.
    Strong Skipping is a mode that improves the runtime performance of your application by skipping unnecessary invocations of composable functions for which the parameters have not changed. In particular, when enabled, Composable functions with unstable parameters become skippable and lambdas with unstable captures will be memoized.
    For more information, see this link:
    Strong skipping
    This feature is enabled by default. To disable, provide this feature flag in a disabled state:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ android {
         }
     }
     composeCompiler {
-        enableStrongSkippingMode = true
         reportsDestination = layout.buildDirectory.dir("compose_compiler")
         // https://developer.android.com/develop/ui/compose/compiler#configuration-options
 //        stabilityConfigurationFile = rootProject.layout.projectDirectory.file("stability_config.conf")
