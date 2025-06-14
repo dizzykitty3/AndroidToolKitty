3df830172908ea71740f2f01ad8a11d3d6739364
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2025-06-14 16:26:36 +0800

    fix license screen (aboutLibraries updated)

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/settings/Licenses.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/settings/Licenses.kt
@@ -1,14 +1,19 @@
 package me.dizzykitty3.androidtoolkitty.settings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.mikepenz.aboutlibraries.ui.compose.android.rememberLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 
 @Composable
 fun Licenses() {
-    Box(Modifier.fillMaxSize()) {
-        LibrariesContainer()
+    Box(Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.background)) {
+        LibrariesContainer(libraries = rememberLibraries().value)
     }
 }
