dec5f90fc8f0537eced7750ee8720bd6fc62ab57
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2025-08-07 09:16:11 +0800

    bump version code (1.10.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "me.dizzykitty3.androidtoolkitty"
         minSdk = 21
         targetSdk = compileSdk
-        versionCode = 1900
-        versionName = "1.9.0"
+        versionCode = 11000
+        versionName = "1.10.0"
 
         resValue("string", "app_name", "ToolKitty")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
