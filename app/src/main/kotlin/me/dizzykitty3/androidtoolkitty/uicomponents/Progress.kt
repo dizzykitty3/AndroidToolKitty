efc416555313a581edbd5886b8ad175ef51e355b
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2025-12-17 09:52:21 +0800

    change: compose material expressive style progress indicator

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/uicomponents/Progress.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/uicomponents/Progress.kt
@@ -1,34 +1,36 @@
 package me.dizzykitty3.androidtoolkitty.uicomponents
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import me.dizzykitty3.androidtoolkitty.utils.DateUtil
 
 @Composable
 fun YearProgressIndicator() {
-    val progress = remember { Animatable(0f) }
-    val targetProgress = DateUtil.yearProgress
-    val durationMillis = 2000
+    var progress by remember { mutableFloatStateOf(0.1f) }
+    val animatedProgress by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec,
+    )
 
     LaunchedEffect(true) {
-        progress.animateTo(
-            targetProgress,
-            animationSpec = tween(durationMillis)
-        )
+        progress = DateUtil.yearProgress
     }
 
     Column(Modifier.fillMaxWidth()) {
         SpacerPadding()
         LinearProgressIndicator(
-            progress = { progress.value },
+            progress = { animatedProgress },
             trackColor = MaterialTheme.colorScheme.surfaceContainer,
             modifier = Modifier.fillMaxWidth()
         )
