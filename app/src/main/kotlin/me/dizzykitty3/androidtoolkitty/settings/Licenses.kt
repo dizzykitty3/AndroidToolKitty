eba096f11e20af51cd0e54abb8fdc4822ef2b8f8
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2026-01-08 10:08:00 +0800

    change: licenses screen layout and colors

--- app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/settings/Licenses.kt
+++ app/src/main/kotlin/me/dizzykitty3/androidtoolkitty/settings/Licenses.kt
@@ -1,29 +1,35 @@
 package me.dizzykitty3.androidtoolkitty.settings
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import com.mikepenz.aboutlibraries.Libs
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import com.mikepenz.aboutlibraries.util.withContext
+import me.dizzykitty3.androidtoolkitty.R
 import me.dizzykitty3.androidtoolkitty.ToolKitty.Companion.appContext
 import me.dizzykitty3.androidtoolkitty.uicomponents.TopBar
 
 @Composable
 fun Licenses(navController: NavHostController) {
-    Box(
-        Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-    ) {
+    Box(Modifier.fillMaxSize()) {
         Column {
-            TopBar(navController = navController)
-            LibrariesContainer(libraries = Libs.Builder().withContext(appContext).build())
+            TopBar(
+                screenTitle = stringResource(R.string.licenses),
+                navController = navController,
+            )
+            Surface(shape = RoundedCornerShape(dimensionResource(R.dimen.rounded_corner_shape))) {
+                LibrariesContainer(
+                    libraries = Libs.Builder().withContext(appContext).build(),
+                )
+            }
         }
     }
 }
