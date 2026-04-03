d0832755cc2a602b3006694640a691b773e4fe62
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2026-04-03 10:50:11 +0800

    lib lifecycle runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,6 +103,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.google.hilt.android)
