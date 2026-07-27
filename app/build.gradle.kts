11514f0056fff37ec538a3b55fe6746a6636dfc7
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2026-07-27 18:48:10 +0800

    v1.22.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     defaultConfig {
         applicationId = "me.dizzykitty3.androidtoolkitty"
         minSdk = 23
-        versionCode = 12100
-        versionName = "1.21.0"
+        versionCode = 12200
+        versionName = "1.22.0"
 
         resValue("string", "app_name", "ToolKitty")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
